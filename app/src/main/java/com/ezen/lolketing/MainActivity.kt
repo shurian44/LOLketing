157d2c008c07510efa6bccf15cda4a2a5c28a298
kmj94102 <kmj94102@naver.com>
2020-02-10 11:54:22 +0900

    ver1.1

--- app/src/main/java/com/ezen/lolketing/MainActivity.kt
+++ app/src/main/java/com/ezen/lolketing/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ezen.lolketing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
