97e3038176cfab5ba8e58fafc95c1d7e767a7257
kmj94102 <kmj94102@naver.com>
2020-02-13 14:13:39 +0900

    ver 1.4

---
+++ app/src/main/java/com/ezen/lolketing/model/GameDTO.kt
@@ -0,0 +1,3 @@
+package com.ezen.lolketing.model
+
+data class GameDTO(var date : String ?= null, var time : String ?= null, var team : String ?= null, var result : String ?= null, var status : String ?= null)
