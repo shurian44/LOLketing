869d211e4d229511b6064d3ba0ebfd089e8b817e
shurian <shurian44@gmail.com>
2020-02-27 17:44:06 +0900

    jjg8.2

--- app/src/main/java/com/ezen/lolketing/BoardDetailActivity.java
+++ app/src/main/java/com/ezen/lolketing/BoardDetailActivity.java
@@ -113,7 +113,6 @@ public class BoardDetailActivity extends AppCompatActivity {
                     }
                 });
                 pc.show();
-
             }
         });
 
@@ -254,6 +253,23 @@ public class BoardDetailActivity extends AppCompatActivity {
                     img_like.setChecked(true);
                 }else{
                     img_like.setChecked(false);
+                }
+                String grade = user.getGrade();
+
+                if(grade.equals("브론즈")){
+                    Glide.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/lolketing.appspot.com/o/bronze.png?alt=media&token=8826d27f-be2d-423e-a4cb-37b6587d914c").into(img_rank);
+                }
+                else if(grade.equals("실버")){
+                    Glide.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/lolketing.appspot.com/o/silver.png?alt=media&token=083c09ba-b311-45de-ba9c-852f2b73afee").into(img_rank);
+                }
+                else if(grade.equals("골드")){
+                    Glide.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/lolketing.appspot.com/o/gold.png?alt=media&token=42ba695d-2f81-43fa-8e6b-c44bda6c827a").into(img_rank);
+                }
+                else if(grade.equals("플래티넘")){
+                    Glide.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/lolketing.appspot.com/o/platinum.png?alt=media&token=e87bbc24-e3dc-4d9b-8695-96dce9e4979e").into(img_rank);
+                }
+                else if(grade.equals("마스터")){
+                    Glide.with(getApplicationContext()).load("https://firebasestorage.googleapis.com/v0/b/lolketing.appspot.com/o/master.png?alt=media&token=72ebd38e-23b2-4462-89ab-598c85d06760").into(img_rank);
                 }
             }
         });
