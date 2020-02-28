7f2c3122efd59bde6029919f18d6139ab3f1f152
shurian <shurian44@gmail.com>
2020-02-28 11:21:38 +0900

    jjg8.4

--- app/src/main/java/com/ezen/lolketing/BoardListActivity.java
+++ app/src/main/java/com/ezen/lolketing/BoardListActivity.java
@@ -2,7 +2,6 @@ package com.ezen.lolketing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -66,17 +65,6 @@ public class BoardListActivity extends AppCompatActivity implements BoardAdapter
         query = firestore.collection("Board").whereEqualTo("team", team).orderBy("timestamp", Query.Direction.DESCENDING);
 
         setRecycler(query);
-
-        main_logo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), JsonActivity.class);
-                intent.setFlags(intent.FLAG_ACTIVITY_SINGLE_TOP |
-                                intent.FLAG_ACTIVITY_CLEAR_TOP |
-                                intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
-                startActivity(intent);
-            }
-        });
 
         board_title.setText("팀 게시판 - " + team);
 
