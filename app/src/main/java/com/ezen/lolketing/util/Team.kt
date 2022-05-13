476ac2fd866fb47ac83f9a7b9572bf5c8f428d6b
김민재 <kmj94102@naver.com>
2022-05-13 22:33:28 +0900

    게임 가이드 수정

--- app/src/main/java/com/ezen/lolketing/util/Team.kt
+++ app/src/main/java/com/ezen/lolketing/util/Team.kt
@@ -1,14 +1,22 @@
 package com.ezen.lolketing.util
 
-enum class Team(name: String) {
-    T1("T1"),
-    GRIFFIN("GRIFFIN"),
-    GENG("Gen.g Esports"),
-    DRAGON("DragonX"),
-    AF("Afreeca Freecs"),
-    SANDBOX("SANDBOX Gamming"),
-    DAMWON("DAMWON Gamming"),
-    APK("APK Prince"),
-    KT("kt Rolster"),
-    HANWHA("Hanwha Life Esports")
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.ezen.lolketing.R
+
+enum class Team(name: String, @DrawableRes val image: Int) {
+    T1("T1", R.drawable.logo_t1),
+    GRIFFIN("GRIFFIN", R.drawable.logo_griffin),
+    GENG("Gen.g Esports", R.drawable.icon_geng),
+    DRAGON("DragonX", R.drawable.icon_dragonx),
+    AF("Afreeca Freecs", R.drawable.icon_afreeca),
+    SANDBOX("SANDBOX Gamming", R.drawable.icon_sandbox),
+    DAMWON("DAMWON Gamming", R.drawable.icon_damwon),
+    APK("APK Prince", R.drawable.icon_apk_prince),
+    KT("kt Rolster", R.drawable.icon_rolster),
+    HANWHA("Hanwha Life Esports", R.drawable.icon_hanwha);
+}
+
+fun setTeamLogoImageView(imageView : ImageView, team : String) {
+    imageView.setImageResource(Team.valueOf(team).image)
 }
--- app/src/main/java/com/ezen/lolketing/view/main/guide/LoLGuideActivity.kt
+++ app/src/main/java/com/ezen/lolketing/view/main/guide/LoLGuideActivity.kt
@@ -4,22 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.ezen.lolketing.BaseActivity
 import com.ezen.lolketing.R
+import com.ezen.lolketing.databinding.ActivityLolGuideBinding
+import com.ezen.lolketing.util.createIntent
+import com.ezen.lolketing.util.startActivity
 import com.ezen.lolketing.view.login.LoginActivity
 import com.ezen.lolketing.view.main.MainActivity
+import com.ezen.lolketing.view.main.manager.ManagerActivity
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class LoLGuideActivity : AppCompatActivity() {
-    private var auth = FirebaseAuth.getInstance()
+@AndroidEntryPoint
+class LoLGuideActivity : BaseActivity<ActivityLolGuideBinding>(R.layout.activity_lol_guide) {
+    @Inject lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lol_guide)
+
+        binding.activity = this
+
     }
 
     // 선택에 맞게 인탠트로 값을 넘겨 상세 페이지로 이동
     fun moveGuide(view: View) {
-        var intent = Intent(this, LoLGuideDetailActivity::class.java)
+        val intent = createIntent(LoLGuideDetailActivity::class.java)
         when(view.id){
             R.id.card1 -> intent.putExtra("status", "aos")
             R.id.card2 -> intent.putExtra("status", "rule")
@@ -31,16 +41,16 @@ class LoLGuideActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun logout(view: View) {
+    /** 로그아웃 버튼 클릭 **/
+    override fun logout(view: View) {
         auth.signOut()
-        var intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
+        startActivity(LoginActivity::class.java, Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        finish()
     }
 
+    /** 관리자 페이지 버튼 클릭 **/
     fun moveHome(view: View) {
-        var intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-        startActivity(intent)
+        startActivity(MainActivity::class.java, Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        finish()
     }
 }
