fdfed42b7be27db3845e5b4875ab68c44433429c
김민재 <kmj94102@naver.com>
2023-12-24 16:10:49 +0900

    - 롤알못 화면 Compose -> Xml 방식으로 변경

--- app/src/main/java/com/ezen/lolketing/view/main/guide/GuideActivity.kt
+++ app/src/main/java/com/ezen/lolketing/view/main/guide/GuideActivity.kt
@@ -1,26 +1,59 @@
 package com.ezen.lolketing.view.main.guide
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.ezen.lolketing.BaseActivity
+import com.ezen.lolketing.R
+import com.ezen.lolketing.databinding.ActivityGuideBinding
+import com.ezen.lolketing.util.createIntent
 import com.ezen.lolketing.view.ui.theme.Black
 import com.ezen.lolketing.view.ui.theme.LOLketingTheme
 
-class GuideActivity : ComponentActivity() {
+class GuideActivity : BaseActivity<ActivityGuideBinding>(R.layout.activity_guide) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LOLketingTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Black
-                ) {
-                    GuideNavigationGraph()
-                }
+
+//                const val STATUS = "status"
+//        const val AOS = "aos"
+//        const val RULE = "rule"
+//        const val POSITION = "position"
+//        const val NATURE = "nature"
+//        const val SCORE = "score"
+        binding.activity = this
+    }
+
+    fun onClick(view: View) {
+        when(view.id) {
+            binding.cardAos.id -> {
+                goToDetail(LoLGuideDetailActivity.AOS)
             }
+            binding.cardRule.id -> {
+                goToDetail(LoLGuideDetailActivity.RULE)
+            }
+            binding.cardPosition.id -> {
+                goToDetail(LoLGuideDetailActivity.POSITION)
+            }
+            binding.cardNature.id -> {
+                goToDetail(LoLGuideDetailActivity.NATURE)
+            }
+            binding.cardScore.id -> {
+                goToDetail(LoLGuideDetailActivity.SCORE)
+
+            }
+            binding.cardTerms.id -> {}
         }
+    }
+
+    private fun goToDetail(value: String) {
+        startActivity(
+            createIntent(LoLGuideDetailActivity::class.java).also {
+                it.putExtra(LoLGuideDetailActivity.STATUS, value)
+            }
+        )
     }
 }
