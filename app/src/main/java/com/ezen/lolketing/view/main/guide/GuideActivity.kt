ab8a0e4d0f43aaa149d5c47655bd8290782405fc
김민재 <kmj94102@naver.com>
2023-12-25 16:06:48 +0900

    - 게임 용어 화면 추가

--- app/src/main/java/com/ezen/lolketing/view/main/guide/GuideActivity.kt
+++ app/src/main/java/com/ezen/lolketing/view/main/guide/GuideActivity.kt
@@ -2,28 +2,16 @@ package com.ezen.lolketing.view.main.guide
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.ezen.lolketing.BaseActivity
 import com.ezen.lolketing.R
 import com.ezen.lolketing.databinding.ActivityGuideBinding
 import com.ezen.lolketing.util.createIntent
-import com.ezen.lolketing.view.ui.theme.Black
-import com.ezen.lolketing.view.ui.theme.LOLketingTheme
+import com.ezen.lolketing.util.startActivity
 
 class GuideActivity : BaseActivity<ActivityGuideBinding>(R.layout.activity_guide) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//                const val STATUS = "status"
-//        const val AOS = "aos"
-//        const val RULE = "rule"
-//        const val POSITION = "position"
-//        const val NATURE = "nature"
-//        const val SCORE = "score"
         binding.activity = this
     }
 
@@ -45,7 +33,9 @@ class GuideActivity : BaseActivity<ActivityGuideBinding>(R.layout.activity_guide
                 goToDetail(LoLGuideDetailActivity.SCORE)
 
             }
-            binding.cardTerms.id -> {}
+            binding.cardTerms.id -> {
+                startActivity(GuideTermsActivity::class.java)
+            }
         }
     }
 
---
+++ app/src/main/java/com/ezen/lolketing/view/main/guide/GuideTermsActivity.kt
@@ -0,0 +1,12 @@
+package com.ezen.lolketing.view.main.guide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ezen.lolketing.R
+
+class GuideTermsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide_terms)
+    }
+}
