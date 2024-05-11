6f083ab53c0c4ef15f93a6f95dd42e94a9389903
김민재 <kmj94102@naver.com>
2024-05-11 19:55:26 +0900

    - 리그 정보, 롤알못 화면 3.0.1 업데이트

--- app/src/main/java/com/ezen/lolketing/view/main/league_info/LeagueInfoActivity.kt
+++ app/src/main/java/com/ezen/lolketing/view/main/league_info/LeagueInfoActivity.kt
@@ -17,7 +17,7 @@ class LeagueInfoActivity : BaseActivity<ActivityLeagueInfoBinding>(R.layout.acti
     } // onCreate()
 
     private fun initViews() = with(binding) {
-        layoutTop.btnBack.setOnClickListener { finish() }
+        activity = this@LeagueInfoActivity
         val list = listOf(getString(R.string.menu_summary), getString(R.string.menu_proceeding), getString(R.string.menu_prize_money))
         val leagueInfoAdapter = LeagueInfoAdapter(this@LeagueInfoActivity).also {
             it.addFragment(OutlineFragment())
